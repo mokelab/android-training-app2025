d12187d257ff9636f7979ead43e62a589a9811fb
Hirokazu Fukami <fkm@mokelab.com>
2025-01-09 02:41:08 +0900

    Add ProgressIndicator

---
+++ core/design/src/main/java/com/mokelab/training/app2025/core/design/Progress.kt
@@ -0,0 +1,14 @@
+package com.mokelab.training.app2025.core.design
+
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ProgressIndicator(
+    modifier: Modifier = Modifier
+) {
+    CircularProgressIndicator(
+        modifier = modifier,
+    )
+}
--- feature/article/src/main/java/com/mokelab/training/app2025/feature/article/ArticleListScreen.kt
+++ feature/article/src/main/java/com/mokelab/training/app2025/feature/article/ArticleListScreen.kt
@@ -1,10 +1,15 @@
 package com.mokelab.training.app2025.feature.article
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.mokelab.training.app2025.core.design.ProgressIndicator
 import com.mokelab.training.app2025.core.design.Screen
 import com.mokelab.training.app2025.core.design.theme.TrainingApp2025Theme
 
@@ -28,7 +33,19 @@ private fun ArticleListScreen(
     Screen(
         modifier = modifier,
     ) { paddingValues ->
-
+        when (uiState) {
+            ArticleListViewModel.UiState.Initial,
+            ArticleListViewModel.UiState.Loading -> {
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier
+                        .padding(paddingValues)
+                        .fillMaxSize(),
+                ) {
+                    ProgressIndicator()
+                }
+            }
+        }
     }
 }
 
