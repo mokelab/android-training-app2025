48e8aec32a2e1a30f840454d2e03506fa6ab9864
Hirokazu Fukami <fkm@mokelab.com>
2025-01-23 15:31:43 +0900

    Add hilt plugin

--- feature/article/build.gradle.kts
+++ feature/article/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
 }
 
 android {
@@ -45,6 +47,7 @@ dependencies {
 
     implementation(libs.androidx.ui.tooling)
     implementation(libs.hilt.android)
+    ksp(libs.hilt.android.compiler)
 
     testImplementation(libs.junit)
     testImplementation(libs.mockk)
